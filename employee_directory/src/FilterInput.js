3d35217ef10363dde79b4445212bde24e2bce66d
Ben Robinson <robinson.dri@gmail.com>
2021-01-24 14:20:34 -0800

    adding api and cols

---
+++ employee_directory/src/FilterInput.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+const FilterInput = ({ users, updateUsers }) => {
+    const [inputValue, updateInput] = useState("");
+
+    useEffect(() => {
+        const filterdUsers = inputValue === ""
+        ? users
+        : users.filter(
+            ({ name: { first }}) =>
+            first.toLowerCase().indexOf(inputValue.toLocaleLowerCase()) >= 0
+        );
+
+        updateUsers(filterdUsers);
+    }, [inputValue, users]);
+
+    return (
+        <input value={inputValue} onChange={e => updateInput(e.target.value)} />
+    );
+};
+
+export default FilterInput;
